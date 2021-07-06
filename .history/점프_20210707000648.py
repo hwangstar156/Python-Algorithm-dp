07f0d9a3266c948cd54cb622b2d800b403bb0230
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-07-07 01:36:33 +0900

    jump

---
+++ .history/점프_20210707000648.py
@@ -0,0 +1,19 @@
+
+
+def dfs(x, y):
+    if x == n-1 and y == n-1:
+        return 1
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        if x+Map[x][y] < n:
+            dp[x][y] += dfs(x+Map[x][y], y)
+        if y+Map[x][y] < n:
+            dp[x][y] += dfs(x, y+Map[x][y])
+    return dp[x][y]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    Map = [list(map(int, input().split()))for _ in range(n)]
+    dp = [[-1]*n for _ in range(n)]
+    print(dfs(0, 0))
