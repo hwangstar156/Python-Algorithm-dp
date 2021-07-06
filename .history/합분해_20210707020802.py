4ebcf981b2c0988504045a473bd08bcc570d35b4
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-07-07 02:09:57 +0900

    합분해

---
+++ .history/합분해_20210707020802.py
@@ -0,0 +1,6 @@
+n, k = map(int, input().split())
+arr = [[1]*(n+1) for _ in range(k+1)]
+for i in range(1, k+1):
+    for j in range(1, n+1):
+        arr[i][j] = (arr[i-1][j]+arr[i][j-1]) % 1000000000
+print(arr[k-1][n-1])
