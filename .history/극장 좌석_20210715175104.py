1db8bdbce4d59ff6c14411caa6c722b7cc7d9e9c
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-07-15 18:00:36 +0900

    Add 극장좌석.py

---
+++ .history/극장 좌석_20210715175104.py
@@ -0,0 +1,17 @@
+arr = []
+arr[1] = 1
+arr[2] = 2
+vip = [0]
+Sum = 1
+n = int(input())
+m = int(input())
+for _ in range(m):
+    vip.append(int(input()))
+vip.append(n+1)
+for i in range(3, 41):
+    arr[i] = arr[i-1]+arr[i-2]
+
+for i in range(len(vip)-1):
+    Sum *= arr[vip[i+1]-vip[i]-1]
+
+print(Sum)
